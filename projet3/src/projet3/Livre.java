fbc670c63f390084d5db4ce47228f637b8af57bb
FUJITSU <FUJITSU@Fujitsu_U748>
2024-02-15 12:20:20 +0100

    ttf

---
+++ projet3/src/projet3/Livre.java
@@ -0,0 +1,29 @@
+package projet3;
+
+public class Livre {
+	private String titre;
+	private String auteur;
+	private int anneePublication;
+	
+	
+	
+	public Livre(String titre,String auteur,int anneePublication) {
+		this.titre= titre;
+		this.auteur= auteur;
+		this.anneePublication= anneePublication;
+	}
+	
+	
+	public void afficherDetails(String titre,String auteur,int anneePublication) {
+		System.out.println("le titre est:"+titre+"et l'auteur est:"+auteur+"et l'annee de publication est:"+anneePublication);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Livre livre1=new Livre("antigone","Anouilh",1944);
+		System.out.println("informations de livre:"+livre1);
+		livre1.afficherDetails("antigone","Anouilh",1944);
+
+	}
+
+}
